67f053012dbbbfd806f29bdf6231d67f9be47e1e
Praneeth <praneethgourla@gmail.com>
2024-05-17 21:24:46 +0530

    add datatypes.js

---
+++ Basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+let name = "praneeth"
+
+let age = 18
+
+let isLoggedIn = false
+
+//Datatypes
+
+// 1) Primitive datatypes
+// number => 2 power 53
+//bigInt for big numbers
+//string => "" or ''
+//boolean => true / false
+//null => standalone value 
+//undefined => if the value is not assigned
+//symbol => unique
+
+
+
+//object we will do it perfectly in nxt sessions
+
+console.log(typeof undefined) //gives undefined as it is a datatype
+console.log(typeof age) //gives number
+console.log(typeof null) //gives object as type
