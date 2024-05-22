6d736e5ab215cbde5e0965b2c40e2cbbeb0b75ca
Praneeth <praneethgourla@gmail.com>
2024-05-22 10:43:39 +0530

    Strings

--- Basics/07-string.js
+++ Basics/07-string.js
@@ -3,12 +3,13 @@ const repoCount = 50
 
 // console.log(name + repoCount + " Value");
 
-console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+// console.log(`Hello iam ${name} and my repository count is ${repoCount}`);
 
 const gameName = new String('hitesh-hc-com')
 
 // console.log(gameName[0]);
-// console.log(gameName.__proto__);
+console.log(gameName.__proto__);
 
 
 // console.log(gameName.length);
