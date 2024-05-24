f0efddb35fc49e6b1e4c18a090b1c5e8745a2fe3
Praneeth <praneethgourla@gmail.com>
2024-05-24 12:17:43 +0530

    for loop

--- 05-Iterations/one.js
+++ 05-Iterations/one.js
@@ -42,7 +42,7 @@ for (let index = 0; index < myArray.length; index++) {
 for (let index = 1; index <= 20; index++) {
     if (index == 5) {
         console.log(`Detected 5`);
-        continue
+        continue  //In JavaScript, the continue keyword is used inside loops (such as for or while loops) to skip the current iteration and proceed to the next one. It's typically used when you want to skip a certain part of the loop's logic based on a condition, but continue with the loop's execution.
     }
    console.log(`Value of i is ${index}`);
     
