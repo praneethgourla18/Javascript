b322eb353f70618700edc01027fb78019623cb2e
Praneeth <praneethgourla@gmail.com>
2024-05-24 11:38:54 +0530

    arrow functions and this keyword

--- 03-Basics/03_arrow.js
+++ 03-Basics/03_arrow.js
@@ -1,23 +1,25 @@
-const user = {
-    username: "hitesh",
-    price: 999,
+// // const user = {
+// //     username: "hitesh",
+// //     price: 999,
 
-    welcomeMessage: function() {
-        console.log(`${this.username} , welcome to website`);
-        console.log(this);
-    }
+// //     welcomeMessage: function() {
+// //         console.log(`${this.username} , welcome to website`);
+    
+// //         console.log(this);  //this will return the object . If we do console.log(this) from another function nested in welcomeMesage that will return the window!!
+      
+// //     }
 
-}
+// // }
 
-// user.welcomeMessage()
-// user.username = "sam"
-// user.welcomeMessage()
+// // // user.welcomeMessage()
+// // user.username = "sam"
+// // user.welcomeMessage()
 
-// console.log(this);
+// console.log(this); //here it refers to a global object in node js environment and window object in browser environment !!
 
 // function chai(){
 //     let username = "hitesh"
-//     console.log(this.username);
+//     console.log(this.username); //returns global object as the function is in non strict mode !! and the op is here is {} and undefined. 
 // }
 
 // chai()
@@ -26,29 +28,31 @@ const user = {
 //     let username = "hitesh"
 //     console.log(this.username);
 // }
-
+  
 const chai =  () => {
     let username = "hitesh"
-    console.log(this);
+    console.log(this)
 }
 
 
-// chai()
+chai()
 
-// const addTwo = (num1, num2) => {
-//     return num1 + num2
-// }
+// // const addTwo = (num1, num2) => {
+// //     return num1 + num2
+// // }
 
-// const addTwo = (num1, num2) =>  num1 + num2
+// // const addTwo = (num1, num2) =>  num1 + num2
 
-// const addTwo = (num1, num2) => ( num1 + num2 )
+// // const addTwo = (num1, num2) => ( num1 + num2 )
 
-const addTwo = (num1, num2) => ({username: "hitesh"})
-
-
-console.log(addTwo(3, 4))
+// // const addTwo = (num1, num2) => ({username: "hitesh"})
 
 
-// const myArray = [2, 5, 3, 7, 8]
+// // console.log(addTwo(3, 4))
 
-// myArray.forEach()
+
+const myArray = [2, 5, 3, 7, 8]
+
+myArray.forEach(function(val){
+    console.log(val);
+})
