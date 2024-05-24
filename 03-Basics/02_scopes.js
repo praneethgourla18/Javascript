c8be66b482e8cc56f21fc02517ce7c74b7ce066f
Praneeth <praneethgourla@gmail.com>
2024-05-24 11:10:59 +0530

    scope in js

--- 03-Basics/02_scopes.js
+++ 03-Basics/02_scopes.js
@@ -1,31 +1,35 @@
-//var c = 300
-let a = 300
-if (true) {
-    let a = 10
-    const b = 20
-    // console.log("INNER: ", a);
+// var c = 300
+// //let a = 300
+
+// if (true) {
+//     let a = 10  //this cannot be accessed outside as let is block scoped
+//     const b = 20  //same as let
+
+//     var d=40;  //this can be accessed oustside as var is function scoped
+//     console.log("INNER: ", a);
     
-}
+// }
 
 
 
 // console.log(a);
-// console.log(b);
+// // console.log(b);
 // console.log(c);
 
 
-function one(){
-    const username = "hitesh"
+// function one(){
+//     const username = "hitesh"
 
-    function two(){
-        const website = "youtube"
-        console.log(username);
-    }
-    // console.log(website);
+//     function two(){
+//         const website = "youtube"
+//         console.log(username);
+//     }
+    
+//     //  console.log(website);
 
-     two()
+//      two()
 
-}
+// }
 
 // one()
 
@@ -33,7 +37,7 @@ if (true) {
     const username = "hitesh"
     if (username === "hitesh") {
         const website = " youtube"
-        // console.log(username + website);
+        console.log(username + website);
     }
     // console.log(website);
 }
@@ -41,18 +45,18 @@ if (true) {
 // console.log(username);
 
 
-// ++++++++++++++++++ interesting ++++++++++++++++++
+// // ++++++++++++++++++ interesting ++++++++++++++++++
 
 
-console.log(addone(5))
+// console.log(addone(5))
 
-function addone(num){
-    return num + 1
-}
+// function addone(num){
+//     return num + 1
+// }
 
 
 
-addTwo(5)
-const addTwo = function(num){
-    return num + 2
-}
+// addTwo(5)
+// const addTwo = function(num){
+//     return num + 2
+// }
