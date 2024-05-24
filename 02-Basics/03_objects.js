8be1c0c912248ad0ec94ec7a3127ee2eaa00ebcb
Praneeth <praneethgourla@gmail.com>
2024-05-24 09:37:32 +0530

    objects in js

--- 02-Basics/03_objects.js
+++ 02-Basics/03_objects.js
@@ -17,19 +17,23 @@ const JsUser = {
     lastLoginDays: ["Monday", "Saturday"]
 }
 
-// console.log(JsUser.email)
-// console.log(JsUser["email"])
+// console.log(JsUser.email) //we can access elements of an object using . operator
+// console.log(JsUser["email"]) //we can also use this to access elements
 // console.log(JsUser["full name"])
 // console.log(JsUser[mySym])
 
-JsUser.email = "hitesh@chatgpt.com"
-// Object.freeze(JsUser)
-JsUser.email = "hitesh@microsoft.com"
+// JsUser.email = "hitesh@chatgpt.com"
+
+// Object.freeze(JsUser)  //The Object.freeze() static method freezes an object. Freezing an object prevents extensions and makes existing properties non-writable and non-configurable. A frozen object can no longer be changed: new properties cannot be added, existing properties cannot be removed, their enumerability, configurability, writability, or value cannot be changed, and the object's prototype cannot be re-assigned. freeze() returns the same object that was passed in.
+// JsUser.email = "hitesh@microsoft.com"
+// // console.log(JsUser.email) 
+
 // console.log(JsUser);
 
 JsUser.greeting = function(){
     console.log("Hello JS user");
-}
+    // return 2; if ntg is specified it will give undefined as output
+}  //these are known as methods  {functions within the object}
 JsUser.greetingTwo = function(){
     console.log(`Hello JS user, ${this.name}`);
 }
