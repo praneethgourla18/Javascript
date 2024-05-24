77cc0e110c1710cbb7ae9d6fc347baa327a1bf19
Praneeth <praneethgourla@gmail.com>
2024-05-24 09:56:43 +0530

    objects in js

--- 02-Basics/04_objects.js
+++ 02-Basics/04_objects.js
@@ -7,15 +7,15 @@ tinderUser.isLoggedIn = false
 
 // console.log(tinderUser);
 
-const regularUser = {
-    email: "some@gmail.com",
-    fullname: {
-        userfullname: {
-            firstname: "hitesh",
-            lastname: "choudhary"
-        }
-    }
-}
+// const regularUser = {
+//     email: "some@gmail.com",
+//     fullname: {
+//         userfullname: {
+//             firstname: "hitesh",
+//             lastname: "choudhary"
+//         }
+//     }
+// }  //an example for nested objects
 
 // console.log(regularUser.fullname.userfullname.firstname);
 
@@ -24,35 +24,35 @@ const obj2 = {3: "a", 4: "b"}
 const obj4 = {5: "a", 6: "b"}
 
 // const obj3 = { obj1, obj2 }
-// const obj3 = Object.assign({}, obj1, obj2, obj4)
+// const obj3 = Object.assign({}, obj1, obj2, obj4) //assign method is used to add elemts separetly
 
-const obj3 = {...obj1, ...obj2}
+// const obj3 = {...obj1, ...obj2} same like array spread operator
 // console.log(obj3);
 
 
-const users = [
-    {
-        id: 1,
-        email: "h@gmail.com"
-    },
-    {
-        id: 1,
-        email: "h@gmail.com"
-    },
-    {
-        id: 1,
-        email: "h@gmail.com"
-    },
-]
+// const users = [
+//     {
+//         id: 1,
+//         email: "h@gmail.com"
+//     },
+//     {
+//         id: 1,
+//         email: "h@gmail.com"
+//     },
+//     {
+//         id: 1,
+//         email: "h@gmail.com"
+//     },
+// ]
 
-users[1].email
-// console.log(tinderUser);
+// console.log(users[0].email)
+// // console.log(tinderUser);
 
-// console.log(Object.keys(tinderUser));
-// console.log(Object.values(tinderUser));
-// console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser)); //returns arr of keys
+// console.log(Object.values(tinderUser)); //returns arr of values
+// console.log(Object.entries(tinderUser)); //returns arr of key value pairs
 
-// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// console.log(tinderUser.hasOwnProperty('isLoggedIn')); //returns whether the object has the specified property or not 
 
 
 const course = {
@@ -65,7 +65,7 @@ const course = {
 
 const {courseInstructor: instructor} = course
 
-// console.log(courseInstructor);
+console.log(courseInstructor); //returns error so you have to define the obj before using it.
 console.log(instructor);
 
 // {
@@ -74,9 +74,9 @@ console.log(instructor);
 //     "price": "free"
 // }
 
-[
-    {},
-    {},
-    {}
-]
+// [
+//     {},
+//     {},
+//     {}
+// ]
 
